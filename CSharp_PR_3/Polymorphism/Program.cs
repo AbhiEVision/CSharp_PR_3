169aa9a58293a69bfdd21afbd95b9eb76261c193
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 11:32:44 +0530

    add polymorphism Program.cs file which contains main method

---
+++ CSharp_PR_3/Polymorphism/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_PR_3.Polymorphism
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Bird myBird = new Bird();
+
+			Bird myDuck = new Duck();
+
+			myBird.Voice();
+
+			myDuck.Voice();
+		}
+	}
+}
