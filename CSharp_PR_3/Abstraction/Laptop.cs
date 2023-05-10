f6cd2753a35ed1005924512e654e367fc0e64cd3
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 11:34:14 +0530

    add : abstraction laptop.cs file which contains main method and other mentioned methods

---
+++ CSharp_PR_3/Abstraction/Laptop.cs
@@ -0,0 +1,47 @@
+﻿namespace CSharp_PR_3.Abstraction
+{
+	public class Laptop
+	{
+		private string brand;
+		private string model;
+
+		public void LaptopDetails()
+		{
+			Console.WriteLine("Brand: " + brand);
+			Console.WriteLine("Model: " + model);
+		}
+
+		private void MotherBoardInfo()
+		{
+			Console.WriteLine("MotherBoard Information");
+		}
+
+		public void SetBrand(string brand)
+		{
+			this.brand = brand;
+		}
+
+		public void SetModel(string model)
+		{
+			this.model = model;
+		}
+
+		public string GetBrand()
+		{
+			return this.brand;
+		}
+		public string GetModel()
+		{
+			return this.model;
+		}
+
+		public static void Main(string[] args)
+		{
+			Laptop laptop = new Laptop();
+			laptop.SetBrand("Dell");
+			laptop.SetModel("Inspiration 5570");
+			laptop.LaptopDetails();
+			laptop.MotherBoardInfo();
+		}
+	}
+}
