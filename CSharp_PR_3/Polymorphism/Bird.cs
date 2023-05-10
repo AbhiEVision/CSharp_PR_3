5b7e696ac0fc35b268cca05c0d1d5e28298d3757
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 11:31:54 +0530

    add : polymorphism Bird.cs file

---
+++ CSharp_PR_3/Polymorphism/Bird.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_PR_3.Polymorphism
+{
+	public class Bird
+	{
+		public void Voice()
+		{
+            Console.WriteLine("Turr Turr");
+        }
+	}
+}
