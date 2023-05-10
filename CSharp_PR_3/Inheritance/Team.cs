9bb007ea75260465721e2ea8d8bc7875cfd30689
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 11:30:42 +0530

    add : inheritance Team.cs file

---
+++ CSharp_PR_3/Inheritance/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_PR_3.Inheritance
+{
+	public class Team : Sponsor
+	{
+		private string teamName = "Gujrat Titans";
+
+		public string PrintInfo()
+		{
+			return teamName + " " + owner;
+		}
+	}
+}
