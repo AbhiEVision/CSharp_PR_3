9e546b50663f1dffb04c78804819dc5ffa147de3
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 11:31:21 +0530

    add : inheritance Program.cs file which contains a main method.

---
+++ CSharp_PR_3/Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_PR_3.Inheritance
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Team team = new();
+            Console.WriteLine(team.PrintInfo());
+        }
+	}
+}
